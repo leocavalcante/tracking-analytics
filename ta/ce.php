d5a2f8a6b9bfee147ac8c96d8efce47d2f3b779f
leocavalcante <lc@leocavalcante.com>
2015-08-27 16:18:40 -0300

    Allows cookie events to redirect too, enforces no redirecting

--- ta/ce.php
+++ ta/ce.php
@@ -3,8 +3,11 @@
 $queryString = $_COOKIE['queryString'];
 $queryString = unserialize($queryString);
 
-$_GET = array_merge($queryString, $_GET);
+if (isset($_GET['r']) && $_GET['r'] == 'no') {
+  unset($queryString['r']);
+  unset($_GET['r']);
+}
 
-unset($_GET['r']);
+$_GET = array_merge($queryString, $_GET);
 
 header('Location: e.php?'.http_build_query($_GET));
